b9c9fb7918be092f94d9136bb54679096bee41d4
alferink <joerg.alferink@web.de>
2017-01-03 13:04:07 +0100

    Initial commit

---
+++ src/main/groovy/de/alferink/BeebeeApplication.groovy
@@ -0,0 +1,12 @@
+package de.alferink
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class BeebeeApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run BeebeeApplication, args
+	}
+}
